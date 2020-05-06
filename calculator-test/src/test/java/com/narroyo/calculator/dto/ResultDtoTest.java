ff26f6564ceeff85a55fdb248ccf12f8a371ccce
PC <PC@DESKTOP-FNBK48H>
2020-05-06 15:55:49 +0200

    Test suite for ResultDto

--- calculator-test/src/test/java/com/narroyo/calculator/dto/ResultDtoTest.java
+++ calculator-test/src/test/java/com/narroyo/calculator/dto/ResultDtoTest.java
@@ -1,5 +1,32 @@
 package com.narroyo.calculator.dto;
 
-public class ResultDtoTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+
+public class ResultDtoTest {
+	@Test
+    public void toStringOK() {
+		
+		
+		ResultDto resultDto = new ResultDto("5.6", true);
+        String dtoAsString = resultDto.toString();
+        assertFalse(dtoAsString.contains("Error"));
+        assertTrue(dtoAsString.contains("5.6"));
+        assertEquals(dtoAsString.toString(),
+                "ResultDto(result=5.6, success=true)");
+    }
+    
+    @Test
+    public void equalsOK() {
+        
+    	ResultDto expected = new ResultDto();
+    	expected.setResult("23");
+    	expected.setSuccess(true);
+    	ResultDto result = new ResultDto("23", true);
+        
+        assertEquals(expected, result);
+    }
 }
